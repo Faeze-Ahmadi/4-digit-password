1e6cf96313e5bac5419ee01acde6495fa1b9ae76
Faeze <faezeahmadi7173@gmail.com>
2024-04-02 18:20:40 +0330

    create def

--- password.py
+++ password.py
@@ -1,3 +1,7 @@
 password = [int(i) for i in input("Enter the 4-digit password: ")]
-if len(password) == 4:
-    print(password)
+
+def password_restrictions(num):
+    if len(password) == 4:
+        return False
+    
+    
