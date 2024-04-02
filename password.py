b3d839a73f8e43d9ec42365bf4e490295fef1eb3
Faeze <faezeahmadi7173@gmail.com>
2024-04-02 18:28:32 +0330

    need to debuge

--- password.py
+++ password.py
@@ -2,6 +2,7 @@ password = [int(i) for i in input("Enter the 4-digit password: ")]
 
 def password_restrictions(num):
     if len(password) == 4:
-        return False
-    
-    
+        if password[2] / password[0] == 4:
+            if password[-1] % 2 != 0:
+                print(password_restrictions(password))
+
