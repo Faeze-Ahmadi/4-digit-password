f311bcb8a7d402a53af421c0526ca0d5ed7e295f
Faeze <faezeahmadi7173@gmail.com>
2024-04-02 18:51:24 +0330

    step 5 of limits

--- password.py
+++ password.py
@@ -5,6 +5,8 @@ def password_restrictions(num):
         if password[2] / password[0] == 4:
             if password[-1] % 2 != 0:
                 if password[0] - 2 < password[-1] < password[0]:
-                    return password
+                    if password[0] == password[1] - 3:
+                        if password[1] / password[0] == 2.5:
+                            return password
 
 print(password_restrictions(password))
